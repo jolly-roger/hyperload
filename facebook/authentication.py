e6742542661fddcdbf1e7373e96595803bc95833
roger <igor.onyshchenko@gmail.com>
2012-03-28 14:06:39 +0300

    Test

--- facebook/authentication.py
+++ facebook/authentication.py
@@ -8,8 +8,10 @@ from . import user
 
 def authenticate(code):
     raw_access_data = str(urllib.request.urlopen("https://graph.facebook.com/oauth/access_token?" \
-        "client_id=" + constants.APP_ID + "&redirect_uri=" + constants.CALLBACK_URL + \
+        "client_id=" + constants.APP_ID  + \
         "&client_secret=" + constants.APP_SECRET + "&code=" + code).read(), encoding="utf-8")
+    
+    #"&redirect_uri=" + constants.CALLBACK_URL + \
     
     access_data = urllib.parse.parse_qs(raw_access_data)
     
