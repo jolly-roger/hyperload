ef113f1f919b72183d1960594c8e1d9ff30fd40b
roger <igor.onyshchenko@gmail.com>
2012-03-28 14:34:57 +0300

    Test

--- facebook/authentication.py
+++ facebook/authentication.py
@@ -6,22 +6,22 @@ from . import constants
 from . import user
 
 
-def authenticate(code):
-    raw_access_data = str(urllib.request.urlopen("https://graph.facebook.com/oauth/access_token?" \
-        "client_id=" + constants.APP_ID  + "&redirect_uri=" + constants.AUTHENTICATE_CALLBACK_URL + \
-        "&client_secret=" + constants.APP_SECRET + "&code=" + code).read(), encoding="utf-8")
-    
-    access_data = urllib.parse.parse_qs(raw_access_data)
-    
-    cherrypy.session[constants.FACEBOOK_ACCESS_TOKEN] = access_data['access_token'][0]
-    #cherrypy.session[constants.FACEBOOK_ACCESS_TOKEN]['expires'] = access_data['expires'][0]
-    
-    user.loadUser(access_data['access_token'][0])
-
 #def authenticate(code):
-#    raise cherrypy.HTTPRedirect("https://graph.facebook.com/oauth/access_token?" \
+#    raw_access_data = str(urllib.request.urlopen("https://graph.facebook.com/oauth/access_token?" \
 #        "client_id=" + constants.APP_ID  + "&redirect_uri=" + constants.AUTHENTICATE_CALLBACK_URL + \
-#        "&client_secret=" + constants.APP_SECRET + "&code=" + code)
+#        "&client_secret=" + constants.APP_SECRET + "&code=" + code).read(), encoding="utf-8")
+#    
+#    access_data = urllib.parse.parse_qs(raw_access_data)
+#    
+#    cherrypy.session[constants.FACEBOOK_ACCESS_TOKEN] = access_data['access_token'][0]
+#    #cherrypy.session[constants.FACEBOOK_ACCESS_TOKEN]['expires'] = access_data['expires'][0]
+#    
+#    user.loadUser(access_data['access_token'][0])
+
+def authenticate(code):
+    raise cherrypy.HTTPRedirect("https://graph.facebook.com/oauth/access_token?" \
+        "client_id=" + constants.APP_ID + "&client_secret=" + constants.APP_SECRET + "&code=" + code + \
+        "&redirect_uri=" + constants.AUTHENTICATE_CALLBACK_URL)
     
 def callbackHandler():    
     access_data = urllib.parse.parse_qs(raw_access_data)
