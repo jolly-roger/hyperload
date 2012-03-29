c1ae177b791fb9ab5a4f5e79bd9d3bfef4f48f38
roger <igor.onyshchenko@gmail.com>
2012-03-29 14:31:31 +0300

    Test

--- facebook/authentication.py
+++ facebook/authentication.py
@@ -12,10 +12,22 @@ def authenticate(code):
     #    "&client_secret=" + constants.APP_SECRET + "&code=" + code,
     #    headers={"User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:11.0) Gecko/20100101 Firefox/11.0"})
     #raw_access_data = str(urllib.request.urlopen(req).read(), encoding="utf-8")
+    
+    
+#    import urllib.request
+#>>> import urllib.parse
+#>>> params = urllib.parse.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0})
+#>>> f = urllib.request.urlopen("http://www.musi-cal.com/cgi-bin/query?%s" % params)
+    
+    params = urllib.parse.urlencode({"client_id": constants.APP_ID, "redirect_uri": constants.AUTHORIZE_CALLBACK_URL,
+        "client_secret": constants.APP_SECRET, "code": code})
+    
+    raw_access_data = str(urllib.request.urlopen("https://graph.facebook.com/oauth/access_token?%s" % params).read(),
+        encoding="utf-8")
         
-    raw_access_data = str(urllib.request.urlopen("https://graph.facebook.com/oauth/access_token?" \
-        "client_id=" + constants.APP_ID  + "&redirect_uri=" + constants.AUTHORIZE_CALLBACK_URL + \
-        "&client_secret=" + constants.APP_SECRET + "&code=" + code).read(), encoding="utf-8")
+    #raw_access_data = str(urllib.request.urlopen("https://graph.facebook.com/oauth/access_token?" \
+    #    "client_id=" + constants.APP_ID  + "&redirect_uri=" + constants.AUTHORIZE_CALLBACK_URL + \
+    #    "&client_secret=" + constants.APP_SECRET + "&code=" + code).read(), encoding="utf-8")
     
     access_data = urllib.parse.parse_qs(raw_access_data)
     
