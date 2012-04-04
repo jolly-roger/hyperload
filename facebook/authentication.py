e19927275293ee48f4857e13433a59e48059bb7e
roger <igor.onyshchenko@gmail.com>
2012-04-04 14:52:56 +0300

    Test

--- facebook/authentication.py
+++ facebook/authentication.py
@@ -24,21 +24,35 @@ def authenticate(code):
     #
     #raw_access_data = str(urllib.request.urlopen("https://graph.facebook.com/oauth/access_token?%s" % params).read(),
     #    encoding="utf-8")
+    
+    params = urllib.parse.urlencode({"client_id": constants.APP_ID, "redirect_uri": constants.AUTHORIZE_CALLBACK_URL,
+        "client_secret": constants.APP_SECRET, "code": code})
+    
+    reqcookies = ""
+    cookie = cherrypy.request.cookie
+
+    for name in cookie.keys():
+        reqcookies += "%s=%s;" % (name, cookie[name].value)
+    
+    req = urllib.request.Request("https://graph.facebook.com:80/oauth/access_token?%s" % params,
+        headers={"Cookie": reqcookies})    
+    
+    raw_access_data = str(urllib.request.urlopen(req).read(), encoding="utf-8")
         
     #raw_access_data = str(urllib.request.urlopen("https://graph.facebook.com/oauth/access_token?" \
     #    "client_id=" + constants.APP_ID  + "&redirect_uri=" + constants.AUTHORIZE_CALLBACK_URL + \
     #    "&client_secret=" + constants.APP_SECRET + "&code=" + code).read(), encoding="utf-8")
     
-    #access_data = urllib.parse.parse_qs(raw_access_data)
+    access_data = urllib.parse.parse_qs(raw_access_data)
     
-    #cherrypy.session[constants.FACEBOOK_ACCESS_TOKEN] = access_data['access_token'][0]
-    ##cherrypy.session[constants.FACEBOOK_ACCESS_TOKEN]['expires'] = access_data['expires'][0]
+    cherrypy.session[constants.FACEBOOK_ACCESS_TOKEN] = access_data['access_token'][0]
+    #cherrypy.session[constants.FACEBOOK_ACCESS_TOKEN]['expires'] = access_data['expires'][0]
     
-    #user.loadUser(access_data['access_token'][0])
+    user.loadUser(access_data['access_token'][0])
     
-    return "https://graph.facebook.com/oauth/access_token?" \
-        "client_id=" + constants.APP_ID  + "&redirect_uri=" + constants.AUTHORIZE_CALLBACK_URL + \
-        "&client_secret=" + constants.APP_SECRET + "&code=" + code
+    #return "https://graph.facebook.com/oauth/access_token?" \
+    #    "client_id=" + constants.APP_ID  + "&redirect_uri=" + constants.AUTHORIZE_CALLBACK_URL + \
+    #    "&client_secret=" + constants.APP_SECRET + "&code=" + code
 
 #def authenticate(code):
 #    params = urllib.parse.urlencode({"client_id": constants.APP_ID, "redirect_uri": constants.AUTHORIZE_CALLBACK_URL,
