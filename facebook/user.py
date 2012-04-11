34fc9e969476f9ad62a526d9bc606582cc77a6ca
roger <igor.onyshchenko@gmail.com>
2012-04-11 10:02:27 +0300

    Test

--- facebook/user.py
+++ facebook/user.py
@@ -5,18 +5,10 @@ import json
 from . import constants
 
 
-USER_URL = "https://graph.facebook.com/me"
 FACEBOOK_USER_ID = "facebook_user_id"
 
 
 def loadUser(userID):
-    #raw_user_data = str(urllib.request.urlopen(USER_URL + "?" \
-    #    "access_token=" + access_token + \
-    #    "&fields=id").read(), encoding="utf-8")
-    #
-    #user_data = json.loads(raw_user_data)
-    #
-    #cherrypy.session[FACEBOOK_USER_ID] = user_data['id']
     cherrypy.session[FACEBOOK_USER_ID] = userID
     
 def getUserId():
