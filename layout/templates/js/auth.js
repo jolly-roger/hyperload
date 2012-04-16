45ac43d518b2f398ddee522f4aca2b055891763d
roger <igor.onyshchenko@gmail.com>
2012-04-16 16:58:39 +0300

    iijkjzsdk

---
+++ layout/templates/js/auth.js
@@ -0,0 +1,32 @@
+if(!hyperload){
+    hyperload = {};
+};
+
+hyperload._auth = function(){
+    this.logout = function(){
+        $.get("/auth/logout", function(data){
+            window.location = data;
+        });
+    };
+
+    this.login = function(loginType){
+        if(loginType == "fb"){
+            FB.login(function(response) {
+                if (response.authResponse && response.authResponse.accessToken &&
+                    response.authResponse.userID) {
+                    $.post( "/auth/login", "accessToken=" + response.authResponse.accessToken +
+                        "&userID=" + response.authResponse.userID, function(data){
+                            if(data){
+                                window.location = data;
+                            }
+                    });
+                } else {
+                  alert('User cancelled login or did not fully authorize.');
+                }
+            });
+        }else if(loginType == "ggl"){
+        };
+    };
+};
+
+hyperload.auth = new hyperload._auth();
--- layout/templates/js/common.js
+++ layout/templates/js/common.js
@@ -46,24 +46,4 @@ function addResource(id, alias, domain, isVerified){
 	});
 };
 
-function logout(){
-    $.get("/auth/logout", function(data){
-        window.location = data;
-    });
-};
-
-function login(){
-    FB.login(function(response) {
-        if (response.authResponse && response.authResponse.accessToken &&
-            response.authResponse.userID) {
-            $.post( "/auth/login", "accessToken=" + response.authResponse.accessToken +
-                "&userID=" + response.authResponse.userID, function(data){
-                    if(data){
-                        window.location = data;
-                    }
-            });
-        } else {
-          alert('User cancelled login or did not fully authorize.');
-        }
-    });
-};
+{% include "js/auth.js"%}
