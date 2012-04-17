a649d6a9ef2b1559030b6dbd5ddc13b02b4868ab
roger <igor.onyshchenko@gmail.com>
2012-04-17 14:56:32 +0300

    Test

--- hyperload.py
+++ hyperload.py
@@ -13,9 +13,6 @@ class hyperload(object):
     resources = resources.resources()
     auth = auth.auth()
     
-    def _cp_on_error(self):
-        cherrypy.response.body = ("Error")    
-    
     @cherrypy.expose
     def index(self, statusid = 0, *args, **kwargs):
         return layout.getIndex()
@@ -35,3 +32,9 @@ class hyperload(object):
 hyperloadconf = os.path.join(os.path.dirname(__file__), "hyperload.conf")
 
 cherrypy.quickstart(hyperload(), config=hyperloadconf)
+
+
+def error_page_default(status, message, traceback, version):
+    return "Error"
+
+cherrypy.config.update({'error_page.default': error_page_default})
