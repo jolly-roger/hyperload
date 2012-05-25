113af2e8aa007c7af8971e7ab28dee78f5e9f92e
roger <igor.onyshchenko@gmail.com>
2012-05-25 14:53:14 +0300

    i

--- remote/remoteJsonEncoder.py
+++ remote/remoteJsonEncoder.py
@@ -1,12 +1,12 @@
 import json
 
-from . import header
-from . import body
-from . import message
+import remote.message
+import remote.header
+import remote.body
 
 
 class remoteJsonEncoder(json.JSONEncoder):
     def default(self, obj):
-        if isinstance(obj, message) or isinstance(obj, header) or isinstance(obj, body):
+        if isinstance(obj, remote.message.message) or isinstance(obj, remote.header.header) or isinstance(obj, remote.body.body):
             return obj.toJSON()
         return json.JSONEncoder.default(self, obj)
